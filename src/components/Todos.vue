caf73e393a7d455b28c5a5d537ece736b4a9d406
Yanis <yanisariouapro@gmail.com>
2020-11-23 15:25:38 +0100

    add new functon addAll

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -5,8 +5,9 @@
       <input type="text" class="new-todo" placeholder="Ajouter une tache" v-model="newTodo" @keyup.enter="addTodo" >
     </header>
     <div class="main">
+      <input type="checkbox" class="toggle-all" v-model="allDone">
       <ul class="todo-list">
-        <li class="todo" v-for="todo in todos" :class="{completed: todo.completed}">
+        <li class="todo" v-for="todo in filteredTodos" :class="{completed: todo.completed}">
           <div class="view">
             <input type="checkbox" v-model="todo.completed">
             <label>{{todo.name}}</label>
@@ -17,9 +18,9 @@
     <footer class="footer">
       <span class="todo-count"><strong>{{remaining}}</strong> Tâche(s) a faire</span>
       <ul class="filters">
-        <li><a href="#" class="{selected: filter === 'all'}">Toutes</a></li>
-        <li><a href="#" class="{selected: filter === 'todo'}">A faire</a></li>
-        <li><a href="#" class="{selected: filter === 'done'}">Faites</a></li>
+        <li><a href="#" :class="{selected: filter === 'all'}" @click.prevent="filter = 'all'">Toutes</a></li>
+        <li><a href="#" :class="{selected: filter === 'todo'}" @click.prevent="filter = 'todo'">A faire</a></li>
+        <li><a href="#" :class="{selected: filter === 'done'}" @click.prevent="filter = 'done'">Faites</a></li>
       </ul>
     </footer>
   </section>
@@ -34,7 +35,7 @@
           completed: false
         }],
         newTodo: '',
-        filter: 'all',
+        filter: 'all'
       }
     },
     methods: {
@@ -44,12 +45,34 @@
           completed: false,
           name: this.newTodo
         })
-        this.newTodo = '',
+        this.newTodo = ''
       }
     },
     computed: {
+      allDone: {
+        get () {
+          return this.remaining === 0
+        },
+        set (value) {
+          if (value === true) {
+            this.todos.forEach(todo => {
+              todo.completed = value
+            })
+          } else {
+
+          }
+        }
+      },
       remaining () {
         return this.todos.filter(todo => !todo.completed).length
+      },
+      filteredTodos () {
+        if (this.filter === 'todo') {
+          return this.todos.filter(todo => !todo.completed)
+        } else if (this.filter === 'done') {
+          return this.todos.filter(todo => todo.completed)
+        }
+        return this.todos
       }
     }
   }
